ca337cbdc17929972b7fef1a9aec64b2d99fef49
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2020-02-06 16:02:08 +0100

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semicolon: 'off',
+    semi: [2, 'never']
   },
+
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser'
   }
-};
+}

a533c8dd75855564e8d25add96ec1141901eff95
Jens Grønhøj Stigaard <jens@stjernholmco.dk>
2020-02-11 15:53:30 +0100

    Changed order of action buttons

--- src/ActionButtons.vue
+++ src/ActionButtons.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 v-row
 	v-col
-		v-btn(block rounded outlined large @click="$emit('exec', { Function: 'Merge' })") Merge
+		v-btn(block rounded outlined large @click="execVmixCommands({ Function: 'Cut' })") Cut
 	v-col
-		v-btn(block rounded outlined large @click="$emit('exec', { Function: 'Fade' })") Fade
+		v-btn(block rounded outlined large @click="execVmixCommands({ Function: 'Merge' })") Merge
 	v-col
-		v-btn(block rounded outlined large @click="$emit('exec', { Function: 'Cut' })") Cut
+		v-btn(block rounded outlined large @click="execVmixCommands({ Function: 'Fade' })") Fade
 	v-col
-		v-btn(block rounded outlined large @click="$emit('exec', { Function: 'QuickPlay' })") Quick Play
+		v-btn(block rounded outlined large @click="execVmixCommands({ Function: 'QuickPlay' })") Quick Play
 </template>
 
 <script lang="ts">
